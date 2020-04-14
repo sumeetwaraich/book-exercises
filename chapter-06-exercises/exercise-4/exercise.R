001d2eb4f0f5076c7b579da9b321499c1a09b6de
sumeetwaraich <62860013+sumeetwaraich@users.noreply.github.com>
2020-04-13 19:26:18 -0700

    finished ch6 ex1,2,3,4

--- chapter-06-exercises/exercise-4/exercise.R
+++ chapter-06-exercises/exercise-4/exercise.R
@@ -4,21 +4,39 @@
 # returns whether or not (e.g., a boolean) the length of one argument is greater
 # than or equal to twice the length of the other.
 # Hint: compare the length difference to the length of the smaller string
-
+is_twice_as_long <- function(str1, str2) {
+  diff <- abs(nchar(str1) - nchar(str2))
+  min_length <- min(nchar(str1), nchar(str2))
+  diff >= min_length # if difference is more than short
+}
 
 # Call your `is_twice_as_long` function by passing it different length strings
 # to confirm that it works. Make sure to check when _either_ argument is twice
 # as long, as well as when neither are!
-
-
+is_twice_as_long("chocolate", "oranges")
+is_twice_as_long("oranges", "chocolate")
+is_twice_as_long("blueberry", "chocolate")
+is_twice_as_long("spider", "dog")
 
 # Define a function `describe_difference` that takes in two strings. The
 # function should return one of the following sentences as appropriate
 #   "Your first string is longer by N characters"
 #   "Your second string is longer by N characters"
 #   "Your strings are the same length!"
-
+describe_difference <- function(first, second) {
+  diff <- nchar(first) - nchar(second)
+  if (diff > 0) {
+    sentence <- paste("Your first string is longer by", diff, "characters")
+  } else if (diff < 0) {
+    sentence <- paste("Your second string is longer by", -diff, "characters")
+  } else {
+    sentence <- "Your strings are the same length!"  
+  }
+  sentence # return sentence 
+}
 
 # Call your `describe_difference` function by passing it different length strings
 # to confirm that it works. Make sure to check all 3 conditions1
-
+describe_difference("chocolate", "oranges")
+describe_difference("oranges", "chocolate")
+describe_difference("chocolate", "blueberry")
