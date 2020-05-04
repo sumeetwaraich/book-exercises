76fef86f419af10a940cff7ac17cc88b369d125e
sumeetwaraich <62860013+sumeetwaraich@users.noreply.github.com>
2020-05-03 21:57:38 -0700

    finished ch 11 ex1,2,3,4,5,6

--- chapter-11-exercises/exercise-4/exercise.R
+++ chapter-11-exercises/exercise-4/exercise.R
@@ -1,7 +1,10 @@
-# Exercise 4: practicing with dplyr
-
+# Exercise 4: practicig with dplyr 
+rm(list = ls())
 # Install the `"nycflights13"` package. Load (`library()`) the package.
 # You'll also need to load `dplyr`
+install.packages("nycflights13")
+library(nycflights13)
+library(dplyr)
 
 
 # The data frame `flights` should now be accessible to you.
@@ -9,36 +12,48 @@
 # What are the names of the columns?
 # Use `??flights` to search for documentation on the data set (for what the 
 # columns represent)
-
+nrow(flights)
+ncol(flights)
+colnames(flights)
+?flights
 
 # Use `dplyr` to give the data frame a new column that is the amount of time
 # gained or lost while flying (that is: how much of the delay arriving occured
 # during flight, as opposed to before departing).
-
+flights <- mutate(flights, delay_in_air = arr_delay - dep_delay)
 
 # Use `dplyr` to sort your data frame in descending order by the column you just
 # created. Remember to save this as a variable (or in the same one!)
-
+flights <- arrange(flights, desc(delay_in_air))
 
 # For practice, repeat the last 2 steps in a single statement using the pipe
 # operator. You can clear your environmental variables to "reset" the data frame
-
+rm(list = ls())
+flights <- flights %>% mutate(delay_in_air = arr_delay - dep_delay) %>% arrange(desc(delay_in_air))
 
 # Make a histogram of the amount of time gained using the `hist()` function
-
+hist(flights$delay_in_air)
 
 # On average, did flights gain or lose time?
 # Note: use the `na.rm = TRUE` argument to remove NA values from your aggregation
-
+mean(flights$delay_in_air, na.rm = TRUE) 
 
 # Create a data.frame of flights headed to SeaTac ('SEA'), only including the
 # origin, destination, and the "gain_in_air" column you just created
-
+headed_Sea <- flights %>% select(origin, dest, delay_in_air) %>% filter(dest == "SEA")
 
 # On average, did flights to SeaTac gain or loose time?
-
+mean(headed_Sea$delay_in_air, na.rm = TRUE)
 
 # Consider flights from JFK to SEA. What was the average, min, and max air time
 # of those flights? Bonus: use pipes to answer this question in one statement
 # (without showing any other data)!
+filter(flights, origin == "JFK", dest == "SEA") %>%
+  summarize(
+    average_time <- mean(air_time, na.rm = TRUE),
+    min_time <- min(air_time, na.rm = TRUE),
+    max_time <- max(air_time, na.rm = TRUE)
+  )
+
+
 
--- chapter-11-exercises/exercise-6/exercise.R
+++ chapter-11-exercises/exercise-6/exercise.R
@@ -10,9 +10,18 @@ library("dplyr")
 # use `left_join()` to join on the "airports" dataframe, which has the airport
 # information
 # Which airport had the largest average arrival delay?
+largest_avg_arrival_delay <- flights %>% 
+  summarize(avg_delay = mean(arr_delay, na.rm = TRUE)) %>%
+  mutate(faa = "dest") %>%
+  left_join(airports, by = "faa") %>%
+  filter(avg_delay == max(avg_delay, na.rm = TRUE))
 
 
 # Create a dataframe of the average arrival delay for each _airline_, then use
 # `left_join()` to join on the "airlines" dataframe
 # Which airline had the smallest average arrival delay?
-
+smallest_avg_arrival_delay <- flights %>%
+  group_by(carrier) %>%
+  summarize(avg_delay = mean(arr_delay, na.rm = TRUE)) %>%
+  left_join(airlines, by = "carrier") %>%
+  filter(avg_delay == max(avg_delay, na.rm = TRUE))
